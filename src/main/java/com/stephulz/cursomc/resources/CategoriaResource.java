c0ff7c462aad2a3e8432649628918475c44aa08a
Stefan Sanches <stefansanches9@gmail.com>
2018-08-20 09:23:08 -0300

    Testing first domain class - Categoria

--- src/main/java/com/stephulz/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/stephulz/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.stephulz.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.stephulz.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest ok";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
