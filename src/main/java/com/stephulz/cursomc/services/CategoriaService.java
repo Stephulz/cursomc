d4ab317fcc24fb87795573869bb2a9f95999dbd6
Stefan Sanches <stefansanches9@gmail.com>
2018-08-21 10:31:53 -0300

    Changes on /categorias/{id} endpoint

--- src/main/java/com/stephulz/cursomc/services/CategoriaService.java
+++ src/main/java/com/stephulz/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.stephulz.cursomc.domain.Categoria;
 import com.stephulz.cursomc.repositories.CategoriaRepository;
+import com.stephulz.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,8 +17,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);		
+		Optional<Categoria> obj = repo.findById(id);  
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));	
 	}
 	
 }
